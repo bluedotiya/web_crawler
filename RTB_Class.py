c7a96aecd255a9e2f7d6964851894596f14dc0ba
bluedotiya <75704012+bluedotiya@users.noreply.github.com>
2021-05-28 01:43:27 +0300

    Update RTB_Class.py

--- RTB_Class.py
+++ RTB_Class.py
@@ -37,7 +37,7 @@ class Buffer:
         self.file_path = file_path
 
     def __str__(self):
-        return "This Class Gets a URL (String) and Saves it on Storage"
+        return "This Class Gets a URL (List) and Saves it on Storage"
 
     def save(self):
         try:
