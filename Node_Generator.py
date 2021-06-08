e353a636b299db853a1816ee6c36feb6f8470df2
bluedotiya <75704012+bluedotiya@users.noreply.github.com>
2021-06-08 20:34:36 +0300

    Visualizer is Working 1!!! just needs to docstring the whole thing and make it more beautiful (the graph)

--- Node_Generator.py
+++ Node_Generator.py
@@ -1,53 +1,86 @@
 import os
+import json
 import math
 import itertools
-for num in range(1, 4):
-    for depth in os.walk(f"Depth{num}").__next__()[1]:
-        for parent in os.walk(f"./Depth{num}/.").__next__()[1]:
-            if os.walk(f"./Depth{num}/{parent}").__next__()[2].__len__() == 2:
-                parent_url = os.walk(f"./Depth{num}/{parent}").__next__()[2][1]
-            else:
-                parent_url = os.walk(f"./Depth{num}/{parent}").__next__()[2][0]
-            with open(f"./Depth{num}/{parent}/{parent_url}", 'r') as parent_file:
-                domain = ''
-                domain_sum = 0
-                theta_angle = 0
-                domain_name = ''
-                top_level_domain = ''
-                node_pose = []
-                parent_file_data = parent_file.readlines()
-                if type(parent_file_data) == list and parent_file_data.__len__() == 2:
-                    url_string, time_delay = parent_file_data[0], parent_file_data[1]
-                    vector_length = time_delay
-                else:
-                    continue
-                url = ((url_string.replace('\n', '').replace('https://', '')).replace('http://', '')).replace('/', '')
-                splited = url.split('.')
-                if splited.__len__() == 3:
-                    sub_domain, domain_name, top_level_domain = splited
-                    for char in f"{domain_name}.{top_level_domain}":
-                        domain_sum += ord(char)
-                    theta_angle = domain_sum % 360
-                    x_pos = math.sin(math.radians(90 - theta_angle)) * float(vector_length)
-                    y_pos = math.sin(math.radians(theta_angle)) * float(vector_length)
-                    if True or 0 <= theta_angle <= 90:
-                        node_pose = f"{x_pos}:{y_pos}"
-                    elif 90 < theta_angle <= 180:
-                        node_pose = f"{-1.0 * x_pos}:{y_pos}"
-                    elif 180 < theta_angle <= 270:
-                        node_pose = f"{-1.0 * x_pos}:{-1.0 * y_pos}"
-                    elif 270 < theta_angle < 360:
-                        node_pose = f"{x_pos}:{-1.0 * y_pos}"
-                    with open("Nodes_Data_duplicated.txt", "a") as nodes_file:
-                        nodes_file.write(
-                            f"{sub_domain}.{domain_name}.{top_level_domain},{node_pose}\n")
-                else:
-                    continue
-count = 0
-with open('Nodes_Data_duplicated.txt', 'rb') as input:
-    with open('Nodes_Data.csv', 'wb') as output:
-        for key,  group in itertools.groupby(sorted(input)):
-            output.write(bytes(str(count) + ',' + str(key, 'ASCII'), 'ASCII'))
-            count += 1
-os.remove("Nodes_Data_duplicated.txt")
 
+
+def generator(depth):
+    for level in range(1, depth + 1):
+        for parent_folder in set([int(name.replace('Parent', '')) for name in os.walk(f"Depth{level}").__next__()[1]]):
+            with open(f"Depth{level}/Parent{parent_folder}/Parent_Url_{parent_folder}.txt", "r") as url_file:
+                file_content = url_file.readlines()
+                file_content_no_newline = [url.replace('\n', '') for url in file_content]
+                if file_content_no_newline[2] != "FAIL":
+                    fcnn = file_content_no_newline
+                    node_dict = vector_calculator(fcnn[0], fcnn[1], fcnn[2])
+                    if node_dict is not None:
+                        with open('Nodes_Data.txt', 'a') as node_file:
+                            json_object = json.dumps(node_dict)
+                            node_file.write(f"{json_object}\n")
+
+
+def vector_calculator(son_url, parent_url, time):
+    domain_sum = 0
+    vector_length = time
+    url_string = son_url
+    url = url_string.split('/')[2]
+    splited = url.split('.')
+    if splited.__len__() == 3:
+        sub_domain, domain_name, top_level_domain = splited
+        for char in f"{domain_name}.{top_level_domain}":
+            domain_sum += ord(char)
+        theta_angle = domain_sum % 360
+        return {"son_url" : {"url" : son_url, "theta" : int(theta_angle), "vector_len" : float(vector_length)},
+                "parent_url" : parent_url}
+
+
+def node_calculator(is_pos_graph):
+    list_of_dicts = []
+    with open('Nodes_Data.txt', 'r') as node_data_file:
+        for data in node_data_file.readlines():
+            json_str = data.replace('\n', '')
+            dict_data = json.loads(json_str)
+            list_of_dicts.append(dict_data)
+        for node_dict in list_of_dicts:
+            son_url = node_dict['son_url']['url']
+            parent_url = node_dict['parent_url']
+            theta_angle = node_dict['son_url']['theta']
+            vector_length = node_dict['son_url']['vector_len']
+            x_pos = math.sin(math.radians(90 - theta_angle)) * float(vector_length)
+            y_pos = math.sin(math.radians(theta_angle)) * float(vector_length)
+            if not is_pos_graph or 0 <= theta_angle <= 90:
+                node_pose = [x_pos, y_pos]
+            elif 90 < theta_angle <= 180:
+                node_pose = [-1.0 * x_pos, y_pos]
+            elif 180 < theta_angle <= 270:
+                node_pose = [-1.0 * x_pos, -1.0 * y_pos]
+            elif 270 < theta_angle < 360:
+                node_pose = [x_pos, -1.0 * y_pos]
+            if parent_url == "Root":
+                with open('Nodes_Position_Data.txt', 'a') as nodes_pos_file:
+                    node_output_data = {"son_url" : {"url" : son_url, "pos" : node_pose}, "parent_url" : parent_url}
+                    nodes_pos_file.write(f"{json.dumps(node_output_data)}\n")
+            else:
+                with open('Nodes_Position_Data.txt', 'r') as nodes_data_file:
+                    for line in nodes_data_file.readlines():
+                        line_dict = json.loads(line)
+                        if son_url == line_dict['son_url']['url']:
+                            node_output_data = line_dict
+                            break
+                        elif parent_url == line_dict['son_url']['url']:
+                            combined_poses = node_pose[0] + line_dict['son_url']['pos'][0],\
+                                             node_pose[1] + line_dict['son_url']['pos'][0]
+                            node_output_data = {"son_url": {"url": son_url, "pos": combined_poses},
+                                                "parent_url": parent_url}
+                            break
+                with open('Nodes_Position_Data.txt', 'a') as nodes_pos_file:
+                    nodes_pos_file.write(f"{json.dumps(node_output_data)}\n")
+
+
+def main():
+    generator(3)
+    node_calculator(False)
+
+
+if __name__ == "__main__":
+    main()
--- networksX.py
+++ networksX.py
@@ -1,31 +1,38 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import csv
+import json
+
 G = nx.Graph()
 
-with open("Nodes_Data.csv", "r") as csv_file:
-    csv_iter = csv.reader(csv_file)
-    for line in csv_iter:
-        x_pos, y_pos = line[2].split(':')
-        domain_name = line[1].split('.')[1] + "." + line[1].split('.')[2]
-        url_link = line[1].split('.')[1]
-        G.add_node(line[0], pos=(float(x_pos), float(y_pos)), size=100, dname=domain_name, url=url_link)
-
+with open("Nodes_Position_Data.txt", "r") as json_file:
+    node_counter = 0
+    for line in json_file:
+        line_dict = json.loads(line)
+        x_pos, y_pos = line_dict['son_url']['pos'][0], line_dict['son_url']['pos'][1]
+        domain_name = line_dict['son_url']['url'].split('.')[-2] + "." + line_dict['son_url']['url'].split('.')[-1]
+        url_link = line_dict['son_url']['url'].split('/')[2]
+        parent_url = line_dict['parent_url']
+        son_url = line_dict['son_url']['url']
+        G.add_node(node_counter, pos=(float(x_pos), float(y_pos)), size=100, dname=domain_name, url=url_link,
+                   parent=parent_url, son=son_url)
+        node_counter += 1
 
 pos = nx.get_node_attributes(G, 'pos')
 size_dict = nx.get_node_attributes(G, 'size')
 size_list = [size for size in size_dict.values()]
-size_outline_list = [size+20 for size in size_dict.values()]
+size_outline_list = [size + 20 for size in size_dict.values()]
 url = nx.get_node_attributes(G, 'url')
-dname_dict = nx.get_node_attributes(G, 'dname')
-for node_num in dname_dict:
-    node_value_count = 0
-    for node_value in dname_dict.values():
-        if dname_dict[node_num] == node_value:
-            G.add_edge(node_num, str(node_value_count))
-        node_value_count += 1
+parent_dict = nx.get_node_attributes(G, 'parent')
+for node_checker in G.nodes.items():
+    for node_runner in G.nodes.items():
+        if node_runner[1]['parent'] == node_checker[1]['son']:
+            start_edge = node_runner[0]
+            end_edge = node_checker[0]
+            G.add_edge(start_edge, end_edge)
+
 nx.draw_networkx(G, pos, labels=url, node_size=size_outline_list, with_labels=True, font_size=6, node_color='r')
 nx.draw_networkx(G, pos, node_size=size_list, with_labels=False, node_color='g')
 nx.draw_networkx_edges(G, pos, width=0.2, alpha=0.5)
-plt.savefig('figure1.png', bbox_inches='tight')
 
+plt.show()
+# plt.savefig('figure1.png', bbox_inches='tight')
