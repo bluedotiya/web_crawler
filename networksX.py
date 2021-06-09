f49f364d1d8ee737274f28aae8de6534216a4541
bluedotiya <75704012+bluedotiya@users.noreply.github.com>
2021-06-09 05:10:22 +0300

    Visualizer is Working 1!!! just needs to docstring the whole thing and make it more beautiful (the graph)

--- networksX.py
+++ networksX.py
@@ -1,38 +1,55 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import json
+import os
 
-G = nx.Graph()
 
-with open("Nodes_Position_Data.txt", "r") as json_file:
-    node_counter = 0
-    for line in json_file:
-        line_dict = json.loads(line)
-        x_pos, y_pos = line_dict['son_url']['pos'][0], line_dict['son_url']['pos'][1]
-        domain_name = line_dict['son_url']['url'].split('.')[-2] + "." + line_dict['son_url']['url'].split('.')[-1]
-        url_link = line_dict['son_url']['url'].split('/')[2]
-        parent_url = line_dict['parent_url']
-        son_url = line_dict['son_url']['url']
-        G.add_node(node_counter, pos=(float(x_pos), float(y_pos)), size=100, dname=domain_name, url=url_link,
-                   parent=parent_url, son=son_url)
-        node_counter += 1
+def create_graph_object():
+    return nx.Graph()
 
-pos = nx.get_node_attributes(G, 'pos')
-size_dict = nx.get_node_attributes(G, 'size')
-size_list = [size for size in size_dict.values()]
-size_outline_list = [size + 20 for size in size_dict.values()]
-url = nx.get_node_attributes(G, 'url')
-parent_dict = nx.get_node_attributes(G, 'parent')
-for node_checker in G.nodes.items():
-    for node_runner in G.nodes.items():
-        if node_runner[1]['parent'] == node_checker[1]['son']:
-            start_edge = node_runner[0]
-            end_edge = node_checker[0]
-            G.add_edge(start_edge, end_edge)
 
-nx.draw_networkx(G, pos, labels=url, node_size=size_outline_list, with_labels=True, font_size=6, node_color='r')
-nx.draw_networkx(G, pos, node_size=size_list, with_labels=False, node_color='g')
-nx.draw_networkx_edges(G, pos, width=0.2, alpha=0.5)
+def data_to_node(graph_object, node_size, nodes_position_file):
+    with open(nodes_position_file, "r") as json_file:
+        node_counter = 0
+        for line in json_file:
+            line_dict = json.loads(line)
+            x_pos, y_pos = line_dict['son_url']['pos'][0], line_dict['son_url']['pos'][1]
+            domain_name = line_dict['son_url']['url'].split('.')[-2]
+            parent_url = line_dict['parent_url']
+            son_url = line_dict['son_url']['url']
+            graph_object.add_node(node_counter, pos=(float(x_pos), float(y_pos)), size=node_size, dname=domain_name,
+                                  url=domain_name , parent=parent_url, son=son_url)
+            node_counter += 1
+    pos = nx.get_node_attributes(graph_object, 'pos')
+    size_dict = nx.get_node_attributes(graph_object, 'size')
+    size_list = [size for size in size_dict.values()]
+    label_dict = nx.get_node_attributes(graph_object, 'url')
+    return pos, size_list, label_dict
 
-plt.show()
-# plt.savefig('figure1.png', bbox_inches='tight')
+
+def nodes_to_edges(graph_object):
+    for node_checker in graph_object.nodes.items():
+        for node_runner in graph_object.nodes.items():
+            if node_runner[1]['parent'] == node_checker[1]['son']:
+                start_edge = node_runner[0]
+                end_edge = node_checker[0]
+                graph_object.add_edge(start_edge, end_edge)
+
+
+def drawer(node_size, nodes_pos_file, font_size, node_color, show_label, save):
+    graph = create_graph_object()
+    pos_dict, size_list, label_dict = data_to_node(graph, node_size, nodes_pos_file)
+    nodes_to_edges(graph)
+    nx.draw(graph, pos_dict, labels=label_dict, node_size=size_list, with_labels=show_label, font_size=font_size, node_color=node_color)
+    if not save:
+        plt.show()
+    else:
+        file_name_count = 1
+        while True:
+            file_name_ext = ".png"
+            if f"{file_name_count}{file_name_ext}" in os.listdir("."):
+                file_name_count += 1
+            else:
+                save_file_name = f"{file_name_count}{file_name_ext}"
+                break
+        plt.savefig(save_file_name, bbox_inches='tight', dpi=400)
