482b5a7361848f583e9f800eb3d562692baac479
oran <ooorannn@gmail.com>
2022-11-26 21:24:38 +0000

    * Added feeder_test to docker file

--- feeder/k8s/kubectl-apply.sh
+++ feeder/k8s/kubectl-apply.sh
@@ -2,6 +2,9 @@
 echo "Create Namespace"
 kubectl apply -f create-ns.yaml
 sleep 3
+echo "Delete Feeder Application"
+kubectl delete -n feeder deployment feeder
+sleep 3
 echo "Create Feeder Application"
 kubectl apply -n feeder -f deployment-file.yaml
 sleep 3
