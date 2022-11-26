6008c2eaf962557d16ee886676c108cc302b25bc
oran <ooorannn@gmail.com>
2022-11-26 21:28:53 +0000

    * Removed sleep

--- feeder/k8s/kubectl-apply.sh
+++ feeder/k8s/kubectl-apply.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 echo "Create Namespace"
 kubectl apply -f create-ns.yaml
-sleep 3
 echo "Delete Feeder Application"
 kubectl delete -n feeder deployment feeder
-sleep 3
 echo "Create Feeder Application"
 kubectl apply -n feeder -f deployment-file.yaml
-sleep 3
 echo "Create Feeder service"
 kubectl apply -n feeder -f expose-service.yaml
-sleep 3
 
