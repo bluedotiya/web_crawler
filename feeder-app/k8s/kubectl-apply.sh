2b9ef2ff47cdb636b0694b9f76cbd99ba5b54382
oran <ooorannn@gmail.com>
2022-11-26 15:19:31 +0000

    * Added bash file to automate applying apps to k8s

---
+++ feeder-app/k8s/kubectl-apply.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Create Namespace"
+kubectl apply -f create-ns.yaml
+sleep 3
+echo "Create Feeder Application"
+kubectl apply -n feeder -f deployment-file.yaml
+sleep 3
+echo "Create Feeder service"
+kubectl apply -n feeder -f expose-service.yaml
+sleep 3
+
