168b35d128c686c8404ed84d7a0bea6c4af6d315
oran dan <ooorannn@gmail.com>
2022-12-07 01:34:16 +0200

    * Implemented integration to Memgraph

--- manager/docker/manager.py
+++ manager/docker/manager.py
@@ -2,6 +2,7 @@ import json
 import jsonschema
 from flask import Flask, request, Response
 import gqlalchemy as gq
+from gqlalchemy.query_builders.memgraph_query_builder import Operator
 import requests
 import re
 
@@ -9,7 +10,7 @@ import re
 MEMGRAPH_DNS_NAME = "memgraph-0.memgraph-svc.default.svc.cluster.local"
 
 # Global Redis connection obj
-db = Memgraph(host=MEMGRAPH_DNS_NAME, port=7687)
+db = gq.Memgraph(host=MEMGRAPH_DNS_NAME, port=7687)
 
 
 # Global Flask server obj
@@ -42,38 +43,85 @@ def get_page_data(url):
         elapsed = response.elapsed
         return str(html_content), elapsed
 
+
 def extract_page_data(html_content):
     """
-    This function job is takes raw HTML Data and extracts URL's from it by using regex.
-    Extracts from the Raw HTML a list of URL's, and extracts it to a dict, output will always be a dict.
+    This function job is takes raw HTML Data and extracts URLs from it by using regex.
+    Extracts from the Raw HTML a list of URLs, and extracts it to a dict, output will always be a dict.
     Returns:
-        A dict containing the URL's extracted from the Raw HTML Data
+        A dict containing the URLs extracted from the Raw HTML Data
     """
     found_list = re.findall("(https?://[\w\-.]+)", html_content)
     response_dict = {'urls': found_list}
     return response_dict
 
 
+def normalize_url(url):
+    return ('.'.join((url.replace('https://', '')).replace('http://', '').split('.')[-1:-3:-1][-1:-3:-1])).upper()
+
+
 @app.route('/', methods=['POST'])
 def index():
     # Check client request header is json
-    if request.is_json == False:
+    if not request.is_json:
         return Response("{'Error':'Only json data is allowed'}", status=400, mimetype='application/json')
     # Validate client request json scheme
     try:
-        schema = {"type" : "object","properties" : {"url" : {"type" : "string"}, "depth" : {"type" : "integer"}}}
+        schema = {"type": "object", "properties": {"url": {"type": "string"}, "depth": {"type": "integer"}}}
         jsonschema.validate(request.get_json(), schema)
     except jsonschema.exceptions.ValidationError:
         return Response("{'Error':'Bad Json scheme'}", status=400, mimetype='application/json')
-    # Check if client request json key is vaild
+    # Check if client request json key is valid
     client_req = request.get_json()
     if list(client_req.keys())[0] != "url":
         return Response("{'Error':'url key was not found in json'}", status=400, mimetype='application/json')
-    
+
     # Remove http/s Scheme from URL, take only Domain name
     requested_url = client_req['url']
-    root_url = ('.'.join((requested_url.replace('https://', '')).replace('http://', '').split('.')[-1:-3:-1][-1:-3:-1])).upper()
-    
-    gq.Merge()
-    client_answer = json.dumps({'Success':'Job started'}), "200"
+    root_url = normalize_url(requested_url)
+
+    # Declare Root Node labels & Node match filter
+    root_labels_dict = {'depth': 'ROOT',
+                        'requested_depth': 'req_depth_3'}
+
+    root_node_labels = [root_labels_dict['depth'],
+                        root_labels_dict['requested_depth']]
+
+    match_filters = root_node_labels
+
+    # Check if URL Root is found on memgraph
+    find_root_node_query = gq.Match(connection=db)\
+                             .node(labels=match_filters, variable='node')\
+                             .where(item='node.name', operator=Operator.EQUAL, literal=root_url)\
+                             .return_()
+
+    match_results = next(find_root_node_query.execute())
+    if match_results.__len__() != 0:
+        return Response("{'Info':'requested url was already searched'}", status=200, mimetype='application/json')
+
+    # Create ROOT URL Node on memgraph
+    gq.Create(db)\
+      .node(labels=root_node_labels, name=root_labels_dict['url'])\
+      .execute()
+
+    # Check if the desired request was successful
+    request_obj = get_page_data(client_req['url'])
+    if request_obj == ('', ''):
+        return Response("{'Error':'Requested URL was not found'}", status=404, mimetype='application/json')
+
+    # Assign html content, request time to vars & Extracting urls from html content
+    request_html, request_time = request_obj
+    extracted_urls = extract_page_data(request_html)
+    for url in extracted_urls['urls']:
+        # Normalize url iterator
+        norm_url = normalize_url(url)
+        # Create node for each URL found and link it to Root Node
+        gq.Merge(db) \
+            .node(labels=root_node_labels, name=root_labels_dict['url']) \
+            .to(relationship_type="Links") \
+            .node(labels=['curr_depth_1', 'req_depth_3', 'processed_0'], name=norm_url) \
+            .execute()
+
+    client_answer = json.dumps({'Success': 'Job started'}), "200"
     return client_answer
+
