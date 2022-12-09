8bd79fd5e04e05ccec957006ff2b9fb8a260f711
Oran <ooorannn@gmail.com>
2022-12-09 19:38:42 +0000

    * Added --- echo

--- helm_app_install.sh
+++ helm_app_install.sh
@@ -3,16 +3,19 @@
 # Uninstall & Install feeder app
 helm uninstall feeder feeder/k8s/
 helm install feeder feeder/k8s/
+echo "---"
 
 # Uninstall & Install manager app
 helm uninstall manager manager/k8s/
 helm install manager manager/k8s/
+echo "---"
 
 # Uninstall & Install memgraphDB app
 helm uninstall memgraph memgraph/k8s/
 helm install memgraph memgraph/k8s/
+echo "---"
 
 # Uninstall & Install memgraph-lab (VNC)
 helm uninstall memgraph-lab memgraph-lab/k8s/
 helm install memgraph-lab memgraph-lab/k8s/
-
+echo "---"
