5f1119c56850b422440e12f9b2c4c7fe62de4e7e
Oran <ooorannn@gmail.com>
2023-01-20 19:49:25 +0000

    * Debug now listen on all interfaces

--- manager/docker/manager.py
+++ manager/docker/manager.py
@@ -7,7 +7,7 @@ import py2neo
 import debugpy # TODO REMOVE ALL DEBUG ONCE READY
 
 # 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
-debugpy.listen(5678)
+debugpy.listen(("0.0.0.0", 5678))
 print("Waiting for debugger attach")
 debugpy.wait_for_client()
 
